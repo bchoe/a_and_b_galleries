478e0503d7cb50c965fee5c7697717c970d6397b
Bryan Choe <bryan.choe88@gmail.com>
2016-10-19 11:26:32 -1000

    working delete and edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,50 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var connect = require('gulp-connect');
+'use strict';
 
-gulp.task('connect', function(){
-  connect.server({
-    root: 'public',
-    livereload: true
+const gulp        = require('gulp');
+const sass        = require('gulp-sass');
+const nodemon     = require('gulp-nodemon');
+const browserSync = require('browser-sync');
+const reload      = browserSync.reload;
+const pkg         = require('./package.json');
+
+gulp.task('nodemon', function (cb) {
+
+  var started = false;
+
+  return nodemon({
+    script : pkg.main,
+    nodeArgs : ['--debug']
+  }).on('start', function () {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+})
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy : 'http://localhost:3000',
+    files : ['public/**/*.*'],
+    browser : 'google chrome',
+    port : 7000
   });
 });
 
-// keeps gulp from crashing for scss errors
 gulp.task('sass', function () {
   return gulp.src('./sass/*.scss')
-      .pipe(sass({ errLogToConsole: true }))
-      .pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('livereload', function (){
-  gulp.src('./public/**/*')
-  .pipe(connect.reload());
+      .pipe(sass({
+        errLogToConsole : true,
+        sourceComments : true,
+        includePaths : ['bower_components/foundation/scss']
+      }).on('error', sass.logError))
+      .pipe(gulp.dest('./public/css'))
+      .pipe(reload({ stream : true }));
 });
 
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./public/**/*', ['livereload']);
+  gulp.watch('./public/**/*.*').on('change', reload);
 });
 
-gulp.task('default', [ 'watch', 'sass']);
+gulp.task('default', ['watch', 'sass', 'browser-sync']);
