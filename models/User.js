235309e831fb2d7777e3e9eaf97968fdb1bed7b8
Bryan Choe <bryan.choe88@gmail.com>
2016-10-21 18:09:38 -1000

    this is it

--- models/User.js
+++ models/User.js
@@ -2,29 +2,11 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     username: DataTypes.STRING,
     password:DataTypes.STRING
-
   }, {
     classMethods: {
       associate: function(models) {
-
       }
     }
   });
-
   return User;
 };
-
-
-//    associate: function(models) {
-//         User.hasMany(models.Photo,{
-//           foreignKey:{
-//             name:'uid',
-//             allowNull:false
-//           }
-//         });
-//       }
-//     }
-//   });
-
-//   return User;
-// };
--- seeders/20161021052020-initializePhotos.js
+++ seeders/20161021052020-initializePhotos.js
@@ -4,49 +4,42 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
 
       return queryInterface.bulkInsert('Photos', [{
-        author: 'Aaron',
-        link: 'http://static2.businessinsider.com/image/562fd7f39dd7cc70408ba0c9-480/drake-hotline-bling.jpg',
-        description: 'Drake',
+        author: 'Hipster',
+        link: 'https://s-media-cache-ak0.pinimg.com/736x/42/a5/e5/42a5e579b47812d968e9357c87d77166.jpg',
+        description: 'not Drake',
         createdAt: new Date(),
         updatedAt: new Date(),
         userId: 1
       },{
-        author: 'Craig Aaron',
-        link: 'http://otherwords.org/wp-content/uploads/2012/12/Craig_Aaron.jpg',
+        author: 'Hawaii',
+        link: 'http://www.astonhotels.com/assets/slides/690x380-Hawaii-Sunset.jpg',
         description: 'Drake Lover',
         createdAt: new Date(),
         updatedAt: new Date(),
         userId: 2
       },{
-        author: 'Aaron',
-        link: 'http://static2.businessinsider.com/image/562fd7f39dd7cc70408ba0c9-480/drake-hotline-bling.jpg',
+        author: 'tmblr',
+        link: 'http://25.media.tumblr.com/tumblr_lel9ozv2Bo1qc9hbio1_500.jpg',
+        description: 'Asian Hipster girl',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: 3
+      },{
+        author: 'azn',
+        link: 'https://s-media-cache-ak0.pinimg.com/originals/28/fe/b6/28feb60cb5c858cf0240df7c0b19e834.jpg',
+        description: 'My girl',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: 4
+      },{
+        author: 'Aaron myspace',
+        link: 'https://a2-images.myspacecdn.com/images01/123/681843da47f756dd2501f6764f0b10d1/300x300.jpg',
         description: 'Drake',
         createdAt: new Date(),
         updatedAt: new Date(),
-        userId: 1
+        userId: 5
       },{
-        author: 'Aaron',
-        link: 'http://static2.businessinsider.com/image/562fd7f39dd7cc70408ba0c9-480/drake-hotline-bling.jpg',
-        description: 'Drake',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },{
-        author: 'Aaron',
-        link: 'http://static2.businessinsider.com/image/562fd7f39dd7cc70408ba0c9-480/drake-hotline-bling.jpg',
-        description: 'Drake',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },{
-        author: 'Aaron',
-        link: 'http://static2.businessinsider.com/image/562fd7f39dd7cc70408ba0c9-480/drake-hotline-bling.jpg',
-        description: 'Drake',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        userId: 1
-      },{
-        author: 'Aaron',
+        author: 'Drake',
         link: 'http://static2.businessinsider.com/image/562fd7f39dd7cc70408ba0c9-480/drake-hotline-bling.jpg',
         description: 'Drake',
         createdAt: new Date(),
